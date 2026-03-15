b276d91d3159988724049692b2c845cddf7f71c6
Tanguy <tanguy.fontaine1@gmail.com>
2026-03-15 02:34:03 +0100

    Fix 'always true shadows' bug on mana symbols

--- frontend/src/components/symbol_button.tsx
+++ frontend/src/components/symbol_button.tsx
@@ -17,9 +17,10 @@ export function SymbolButton(props: SymbolButtonProps)
 {
    const { setValue, symbol, symbolOnly, shadow, ...otherProps } = props;
 
+   // ?? => if shadow is undefined or null, it will be set to false
    return (
       <Box as="button" fontSize={20} onClick={setValue} {...otherProps}>
-         <Symbol symbol={symbol} symbolOnly={symbolOnly} shadow={shadow || true} />
+         <Symbol symbol={symbol} symbolOnly={symbolOnly} shadow={shadow ?? false} />
       </Box>
    );
 }
